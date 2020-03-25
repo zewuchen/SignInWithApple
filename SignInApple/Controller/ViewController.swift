1d5d4a5730338fdda4ca293c9f45d026c76282ab
Zewu Chen <zewu.chen@etec.sp.gov.br>
2020-03-25 18:03:39 -0300

    Add alert to show information and login view

--- SignInApple/Controller/ViewController.swift
+++ SignInApple/Controller/ViewController.swift
@@ -29,6 +29,7 @@ extension ViewController: ASAuthorizationControllerDelegate {
         let request = appleIDProvider.createRequest()
         request.requestedScopes = [.fullName, .email]
         let authorizationController = ASAuthorizationController(authorizationRequests: [request])
+        authorizationController.presentationContextProvider = self
         authorizationController.delegate = self
         authorizationController.performRequests()
     }
@@ -38,9 +39,21 @@ extension ViewController: ASAuthorizationControllerDelegate {
             let userIdentifier = appleIDCredential.user
             let fullName = appleIDCredential.fullName
             let email = appleIDCredential.email
+            let alert = UIAlertController(title: "Authorization Success", message: "\(userIdentifier) \n \(String(describing: fullName)) \n \(email)", preferredStyle: .alert)
+            alert.addAction(UIAlertAction(title: "Close", style: .default, handler: nil))
+            self.present(alert, animated: true, completion: nil)
             print("User id is \(userIdentifier) \n Full Name is \(String(describing: fullName)) \n Email id is \(String(describing: email))") }
     }
     func authorizationController(controller: ASAuthorizationController, didCompleteWithError error: Error) {
-        print(error)
+        let alert = UIAlertController(title: "Authorization Error", message: error.localizedDescription, preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "Close", style: .default, handler: nil))
+        self.present(alert, animated: true, completion: nil)
+    }
+}
+
+extension ViewController: ASAuthorizationControllerPresentationContextProviding {
+    // MARK: It’s responsible for presenting the Apple ID login view, so you can simply return the window of your app
+    func presentationAnchor(for controller: ASAuthorizationController) -> ASPresentationAnchor {
+        return self.view.window!
     }
 }
